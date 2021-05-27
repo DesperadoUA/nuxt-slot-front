51c5269565017d620f19af905293d072c06db6fd
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2021-05-27 18:07:59 +0300

    Fixes

--- client/components/intro/app-intro.vue
+++ client/components/intro/app-intro.vue
@@ -26,23 +26,23 @@
 
 <style lang="scss">
 .intro {
-  font-size: 14px;
-  line-height: 1.8333;
-  color: var(--theme-cr-txt-alt);
-  margin-bottom: 25px;
+    font-size: 14px;
+    line-height: 1.8333;
+    color: var(--theme-cr-txt-alt);
+    margin-bottom: 25px;
 
-  @media (min-width: 992px) {
-    margin-bottom: 50px;
-    font-size: 18px;
-  }
+    @media (min-width: 992px) {
+        margin-bottom: 50px;
+        font-size: 18px;
+    }
 }
 
 .intro__sub-heading {
-  font-size: 24px;
-  line-height: 1.25;
+    font-size: 24px;
+    line-height: 1.25;
 
-  @media (min-width: 992px) {
-    font-size: 32px;
-  }
+    @media (min-width: 992px) {
+        font-size: 32px;
+    }
 }
 </style>
