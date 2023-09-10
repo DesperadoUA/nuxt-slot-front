3cbf702ad14527dfbf6cbaf6fbb57a6457cdffae
lazarev <lazarev-konstant@mail.ru>
2023-09-10 12:40:49 +0300

    Add loader amp script for news

--- client/pages/amp/news/index.vue
+++ client/pages/amp/news/index.vue
@@ -3,7 +3,13 @@
 		<app_header_amp :logo="data.body.options.logo" :menu_links="data.body.settings.header_menu" />
 		<main class="main">
 			<app_intro_amp :value="data.body" />
-			<app_news_loop_amp :value="data.body.news" />
+			<script_amp :src="NewsPathScript" v-if="NewsNumberPostOnQuery < data.body.news.length">
+				<app_news_loop_amp :value="data.body.news" />
+			</script_amp>
+			<app_news_loop_amp
+				:value="data.body.news"
+				v-if="data.body.news.length !== 0 && NewsNumberPostOnQuery > data.body.news.length"
+			/>
 			<app_content_amp :value="data.body.amp_content" />
 		</main>
 		<app_footer_amp :footer_menu="data.body.settings.footer_menu" :footer_text="data.body.settings.footer_text" />
@@ -16,9 +22,18 @@ import app_intro_amp from '~/components/intro/app-intro_amp'
 import app_news_loop_amp from '~/components/news_loop/app_news_loop_amp'
 import pageTemplateAmp from '~/mixins/pageTemplateAmp'
 import helper from '~/helpers/helpers'
+import script_amp from '~/components/script_amp'
+import { NEWS as NewsNumberPostOnQuery } from '~/config/postLoader'
+import { NEWS as NewsPathScript } from '~/config/ampPathScript'
 export default {
 	name: 'index_news_amp',
-	components: { app_intro_amp, app_news_loop_amp },
+	components: { app_intro_amp, app_news_loop_amp, script_amp },
+	data: () => {
+		return {
+			NewsNumberPostOnQuery,
+			NewsPathScript
+		}
+	},
 	mixins: [pageTemplateAmp],
 	async asyncData({ route, error }) {
 		const request = new DAL_Builder()
